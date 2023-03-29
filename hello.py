ed5840cc68612c10de62bc1f65d4e5d9217b54da
Andr35 <silverback.wolf.207@gmail.com>
2023-03-28 21:51:11 -0500

    modificaciones en python

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print('hello world')
+
+def function(a, b):
+    if a < b :
+        print("a debe ser mayor a b")
+    else :
+        if a % b == 0 :
+            print("es par")
+        if a % b == 1 :
+            print("es impar")
+
+
+function(10,5)
